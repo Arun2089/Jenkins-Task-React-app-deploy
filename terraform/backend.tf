50068c4d471647325e2a602c652daedcb74748db
Arun2089 <arunlohar@gkmit.co>
2024-08-20 02:54:51 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "backend-terraform-94144"
-    key            = "Jenkins/terraform.tfstate"
-    region         = "ap-south-1" 
-    encrypt        = true
-  }
-}
